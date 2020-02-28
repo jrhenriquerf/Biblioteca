d8880f86f7cdf8a2cb8f6a1f7c0916833edc09dd
Jair Henrique Rangel Francisco <jfrancisco@localhost.localdomain>
2020-02-28 01:17:03 -0300

    lendings and give backs

---
+++ src/resources/views/lending/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Pendings</div>
+
+                <div class="card-body">
+                    <form class="form-inline" action="{{ route('lending.search') }}" method="GET">
+                        {{ csrf_field() }}                      
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="inputSearch" placeholder="Search your pending"> 
+                        </div>
+                        <div class="col-sm-3">
+                            <button type="submit" class="btn btn-outline-secondary"><i class="fa fa-search"></i> Search</button>
+                        </div>
+                    </form>
+                    <br>
+                    <table class="table table-hover">
+                        <thead class="thead-dark">
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Book</th>
+                            <th scope="col">Start Date</th>
+                            <th scope="col">End Date</th>
+                            <th scope="col" class="text-center">Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            <?php $i = 1; ?>
+                            @foreach ($lendings as $pending)
+                                <tr>
+                                    <th scope="row">{{ $i }}</th>
+                                    <td>{{ $pending->books[0]->title }}</td>
+                                    <td>{{ $pending->date_start }}</td>
+                                    <td>{{ $pending->date_end }}</td>
+                                    <td width="100" class="text-center">
+                                        <form action="{{ route('lending.update', $pending->id) }}" method="POST" style="display: inline-block">
+                                            {{ method_field('PUT') }}
+                                            {{ csrf_field() }}
+                                            <button type="submit" class="btn btn-outline-danger btn-sm" style="width:100px">Give back</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                <?php $i++; ?>
+                            @endforeach
+                        </tbody>
+                    </table>               
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
