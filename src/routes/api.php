2bbc325871d81f644c1c60387769f290e42539f0
Jair Henrique Rangel Francisco <jfrancisco@localhost.localdomain>
2020-03-19 02:21:17 -0300

    Configuração do Cors como middleware para as rotas de api

--- src/routes/api.php
+++ src/routes/api.php
@@ -13,11 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('cors', 'auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth.token')->prefix('/v1')->group(function () {
+Route::middleware('cors', 'auth.token')->prefix('/v1')->group(function () {
     Route::get('books', 'Api\\ApiController@books');
     Route::get('authors', 'Api\\ApiController@authors');
     Route::post('save_book', 'Api\\ApiController@saveBook');
